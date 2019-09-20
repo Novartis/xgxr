390e9cc2e12ff3f9bda46b3cefb419862feef30e
Andy Stein <andrew.stein@novartis.com>
2019-09-20 11:44:05 -0400

    rerun usethis::use_data using ata_create/usethis_script.R

---
+++ data_create/usethis_script.R
@@ -0,0 +1,18 @@
+case1_pkpd = read.csv("data_create/raw/case1_pkpd.csv", stringsAsFactors = FALSE)
+usethis::use_data(case1_pkpd, overwrite = TRUE)
+
+mad = read.csv("data_create/raw/mad.csv", stringsAsFactors = FALSE)
+usethis::use_data(mad, overwrite = TRUE)
+
+mad_missing_duplicates = read.csv("data_create/raw/mad_missing_duplicates.csv", stringsAsFactors = FALSE)
+usethis::use_data(mad_missing_duplicates, overwrite = TRUE)
+
+mad_nca = read.csv("data_create/raw/mad_nca.csv", stringsAsFactors = FALSE)
+usethis::use_data(mad_nca, overwrite = TRUE)
+
+nlmixr_theo_sd = read.csv("data_create/raw/nlmixr_theo_sd.csv", stringsAsFactors = FALSE)
+usethis::use_data(nlmixr_theo_sd, overwrite = TRUE)
+
+sad = read.csv("data_create/raw/sad.csv", stringsAsFactors = FALSE)
+usethis::use_data(sad, overwrite = TRUE)
+
