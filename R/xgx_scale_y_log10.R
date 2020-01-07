36d5f6d5615268cfd2aea8552cd3ca86c940daf8
margoal1 <44787126+margoal1@users.noreply.github.com>
2020-01-07 16:34:29 -0500

    Update xgx_scale_y_log10.R
    Streamline minor_breaks definition across scaling functions by using helper function xgx_minor_breaks_log10()

--- R/xgx_scale_y_log10.R
+++ R/xgx_scale_y_log10.R
@@ -32,20 +32,7 @@ xgx_scale_y_log10 <-  function(breaks = xgx_breaks_log10,
                                labels = xgx_labels_log10,
                                ...) {
   if (is.null(minor_breaks)) {
-    minor_breaks <-  function(x) {
-      r1 <- range(log10(x))
-      r <-  r1
-      r[1] <-  floor(r[1])
-      r[2] <-  ceiling(r[2]) + 1
-      breaks <- c()
-      for (i in seq(r[1], r[2])) {
-        breaks <-  c(breaks, seq(2 * 10^(i - 1), 10^i - 10^(i - 1),
-                                 by = 10^(i - 1)))
-      }
-      breaks <-  breaks[breaks <= 10^r1[2]]
-      breaks <-  breaks[breaks >= 10^r1[1]]
-      return(breaks)
-    }
+    minor_breaks <-  function(x) xgx_minor_breaks_log10(x)
   }
 
   ret <- try(list(ggplot2::scale_y_log10(..., breaks = breaks,
