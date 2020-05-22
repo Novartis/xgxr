2c5746419601ff72aafa9d4de6f45783d2a37a7a
MARGOAL1 <alison.margolskee@novartis.com>
2020-05-22 19:27:30 +0200

    correct documentation for predictdf.nls

--- Rdev/xgx_geom_smooth_emax.R
+++ Rdev/xgx_geom_smooth_emax.R
@@ -71,7 +71,7 @@ xgx_geom_smooth_emax <- function(mapping = NULL, data = NULL, geom = "smooth",
 
 
 #' Prediction data frame for nls
-#' Get predictions with standard errors into data frame
+#' Get predictions with standard errors into data frame for use with geom_smooth
 #' 
 #' \code{predictdf.nls} calculates CI for a model fit of class nls based on the "delta-method" 
 #' http://sia.webpopix.org/nonlinearRegression.html#confidence-intervals-and-prediction-intervals)
@@ -91,36 +91,6 @@ xgx_geom_smooth_emax <- function(mapping = NULL, data = NULL, geom = "smooth",
 #' @param level Level of confidence interval to use
 #'
 #' @return dataframe with x and y values, if se is TRUE dataframe also includes ymin and ymax
-#'
-#' @examples 
-#' Nsubj <- 10
-#' Doses <- c(0, 25, 50, 100, 200)
-#' Ntot <- Nsubj*length(Doses)
-#' times <- c(0,14,30,60,90)
-#' 
-#' dat1 <- data.frame(ID = 1:(Ntot),
-#'                    DOSE = rep(Doses, Nsubj),
-#'                    PD0 = rlnorm(Ntot, log(100), 1),
-#'                    Kout = exp(rnorm(Ntot,-2, 0.3)),
-#'                    Imax = 1,
-#'                    ED50 = 25) %>%
-#'   dplyr::mutate(PDSS = PD0*(1 - Imax*DOSE/(DOSE + ED50))*exp(rnorm(Ntot, 0.05, 0.3))  ) %>%
-#'   merge(data.frame(ID = rep(1:(Ntot), each = length(times)), Time = times), by = "ID") %>%
-#'   dplyr::mutate(PD = ((PD0 - PDSS)*(exp(-Kout*Time)) + PDSS), 
-#'                 PCHG = (PD - PD0)/PD0)
-#' 
-#' gg <- ggplot2::ggplot(dat1 %>% subset(Time == 90), 
-#'                       ggplot2::aes(x = DOSE, y = PCHG)) +
-#'   ggplot2::geom_boxplot(aes(group = DOSE)) +
-#'   xgx_theme() +
-#'   # xgx_scale_y_percentchangelog10() +
-#'   ylab("Percent Change from Baseline") +
-#'   xlab("Dose (mg)")
-#' 
-#' gg + 
-#'   geom_smooth(method = "nls", formula = y ~ Emax*x/(ED50 + x), 
-#'               method.args = list(start = list(Emax = -0.50, ED50 = 25)), 
-#'               se = TRUE, color = "black")
 #'
 #' @importFrom stats deriv
 #' @importFrom ggplot2 predictdf.default
