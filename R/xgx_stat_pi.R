f908c794c498eef49798b17af892a1ec288dba5b
MARGOAL1 <alison.margolskee@novartis.com>
2020-07-02 22:27:09 +0200

    clean up stat pi

--- R/xgx_stat_pi.R
+++ R/xgx_stat_pi.R
@@ -113,40 +113,6 @@ xgx_stat_pi <- function(mapping = NULL, data = NULL, percent_level = 0.95,
                      show.legend = show.legend,
                      inherit.aes = inherit.aes,
                      ...)
-  
-  # ret <- list()
-  # for (igeom in geom) {
-  #   temp <- ggplot2::stat_summary(mapping = mapping, data = data,
-  #                                 geom = igeom, position = position, ...,
-  #                                 fun.args = list(), na.rm = na.rm,
-  #                                 show.legend = show.legend,
-  #                                 inherit.aes = inherit.aes,
-  #                                 fun.data = function(y) percent_int(y, percent_level)
-  #   )
-  #   
-  #   if (igeom == "point") {
-  #     if (is.null(temp$aes_params$size)) temp$aes_params$size <- 2
-  #   }
-  #   else if (igeom == "line") {
-  #     if (is.null(temp$aes_params$size)) temp$aes_params$size <- 1
-  #   }
-  #   else if (igeom == "errorbar") {
-  #     if (is.null(temp$aes_params$size)) temp$aes_params$size <- 1
-  #     if (is.null(temp$geom_params$width)) {
-  #       temp$geom_params$width <- 0
-  #     }
-  #   }
-  #   else if (igeom == "ribbon") {
-  #     if(is.null(temp$aes_params$alpha)) temp$aes_params$alpha <- 0.25
-  #   }
-  #   else if (igeom == "pointrange") {
-  #     if(is.null(temp$aes_params$size)) temp$aes_params$size <- 1
-  #     temp$geom$geom_params$fatten <- 2
-  #   }
-  #   
-  #   
-  #   ret[[paste0("geom_", igeom)]] <- temp
-  # }
-  
+
   return(ret)
 }
