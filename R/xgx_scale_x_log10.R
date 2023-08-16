d7e947c36c519396f6fa4c6537ef11fd7b1f1b64
Bill Denney <wdenney@humanpredictions.com>
2023-08-15 21:17:43 -0400

    make xgx_scale_x_log10 and xgx_scale_y_log10 return ggproto instead of sometimes returning a list

--- R/xgx_scale_x_log10.R
+++ R/xgx_scale_x_log10.R
@@ -1,30 +1,30 @@
 #' log10 scales the x axis with a "pretty" set of breaks
-#' 
+#'
 #' \code{xgx_scale_x_log10} is similar to
-#' \code{\link[ggplot2:scale_continuous]{scale_x_log10}}.  
+#' \code{\link[ggplot2:scale_continuous]{scale_x_log10}}.
 #' But it uses what we believe to be a nicer spacing and set of tick marks
 #' it can be used the same as
 #' \code{\link[ggplot2:scale_continuous]{scale_x_log10}}
 #'
 #' @param breaks major breaks, default is a function defined here
 #' @param minor_breaks minor breaks, default is a function defined here
-#' @param labels function for setting the labels, defined here 
+#' @param labels function for setting the labels, defined here
 #' @param ... other arguments passed to
 #' \code{\link[ggplot2:scale_continuous]{scale_x_log10}}
 #'
 #' @return ggplot2 compatible scale object
-#' 
-#' @examples  
+#'
+#' @examples
 #' conc <- 10^(seq(-3, 3, by = 0.1))
 #' ec50 <- 1
 #' data <- data.frame(concentration  = conc,
 #'                    bound_receptor = 1 * conc / (conc + ec50))
-#' ggplot2::ggplot(data, ggplot2::aes(x = concentration, y = bound_receptor)) + 
-#' ggplot2::geom_point() + 
-#'   ggplot2::geom_line() + 
+#' ggplot2::ggplot(data, ggplot2::aes(x = concentration, y = bound_receptor)) +
+#' ggplot2::geom_point() +
+#'   ggplot2::geom_line() +
 #'   xgx_scale_x_log10() +
 #'   xgx_scale_y_reverselog10()
-#'   
+#'
 #' @importFrom ggplot2 scale_x_log10
 #' @export
 xgx_scale_x_log10 <-  function(breaks = xgx_breaks_log10,
@@ -35,10 +35,16 @@ xgx_scale_x_log10 <-  function(breaks = xgx_breaks_log10,
     minor_breaks <- function(x) xgx_minor_breaks_log10(x)
   }
 
-  ret <- try(list(ggplot2::scale_x_log10(..., breaks = breaks,
-                                         minor_breaks = minor_breaks,
-                                         labels = labels)),
-             silent = TRUE)
+  ret <-
+    try(
+      ggplot2::scale_x_log10(
+        ...,
+        breaks = breaks,
+        minor_breaks = minor_breaks,
+        labels = labels
+      ),
+      silent = TRUE
+    )
   if  (inherits(ret, "try-error")) {
     return(ggplot2::scale_x_log10(...))
   } else {
--- R/xgx_scale_y_log10.R
+++ R/xgx_scale_y_log10.R
@@ -1,30 +1,30 @@
 #' log10 scales the y axis with a "pretty" set of breaks
-#' 
-#' \code{xgx_scale_y_log10} is similar to 
-#' \code{\link[ggplot2:scale_continuous]{scale_y_log10}}.  
+#'
+#' \code{xgx_scale_y_log10} is similar to
+#' \code{\link[ggplot2:scale_continuous]{scale_y_log10}}.
 #' But it uses what we believe to be a nicer spacing and set of tick marks
-#' it can be used the same as 
+#' it can be used the same as
 #' \code{\link[ggplot2:scale_continuous]{scale_y_log10}}
 #'
 #' @param breaks major breaks, default is a function defined here
 #' @param minor_breaks minor breaks, default is a function defined here
-#' @param labels function for setting the labels, defined here 
-#' @param ... other arguments passed to 
+#' @param labels function for setting the labels, defined here
+#' @param ... other arguments passed to
 #' \code{\link[ggplot2:scale_continuous]{scale_y_log10}}
 #'
 #' @return ggplot2 compatible scale object
-#' 
-#' @examples 
+#'
+#' @examples
 #' conc <- 10^(seq(-3, 3, by = 0.1))
 #' ec50 <- 1
-#' data <- data.frame(concentration = conc, 
+#' data <- data.frame(concentration = conc,
 #'                    bound_receptor = 1 * conc / (conc + ec50))
 #' ggplot2::ggplot(data, ggplot2::aes(y = concentration, x = bound_receptor)) +
 #'   ggplot2::geom_point() +
 #'   ggplot2::geom_line() +
 #'   xgx_scale_y_log10() +
 #'   xgx_scale_x_reverselog10()
-#'   
+#'
 #' @importFrom ggplot2 scale_y_log10
 #' @export
 xgx_scale_y_log10 <-  function(breaks = xgx_breaks_log10,
@@ -32,13 +32,19 @@ xgx_scale_y_log10 <-  function(breaks = xgx_breaks_log10,
                                labels = xgx_labels_log10,
                                ...) {
   if (is.null(minor_breaks)) {
-    minor_breaks <-  function(x) xgx_minor_breaks_log10(x)
+    minor_breaks <- function(x) xgx_minor_breaks_log10(x)
   }
 
-  ret <- try(list(ggplot2::scale_y_log10(..., breaks = breaks,
-                                         minor_breaks = minor_breaks,
-                                         labels = labels)),
-             silent = TRUE)
+  ret <-
+    try(
+      ggplot2::scale_y_log10(
+        ...,
+        breaks = breaks,
+        minor_breaks = minor_breaks,
+        labels = labels
+      ),
+      silent = TRUE
+    )
   if  (inherits(ret, "try-error")) {
     return(ggplot2::scale_y_log10(...))
   } else {
