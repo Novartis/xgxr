332dd516dc1f5251e64b58fa9e2b7e45e045186a
iamstein <andy.stein@gmail.com>
2023-09-29 17:39:41 -0400

    renamed dummy_data to data

--- R/xgx_annotate_status.R
+++ R/xgx_annotate_status.R
@@ -30,7 +30,7 @@
 #'   
 #' # for dates
 #'   data <- data.frame(x = as.Date(c("2015-01-01", "2016-01-01")), y = c(1,2))
-#'   xmax <- max(na.omit(dummy_data$x))
+#'   xmax <- max(na.omit(data$x))
 #'   ggplot2::ggplot(data, aes(x = x, y = y)) + geom_point() + xgx_annotate_status("DRAFT", x = xmax)
 #' 
 #' @importFrom ggplot2 ggplot
